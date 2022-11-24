fcfeb3d04c756b6e34f13402ad7992091db1d88d
marcelinoandika <117901348+marcelinoandika@users.noreply.github.com>
2022-11-24 16:15:25 +0700

    Update Soal 1_D_71220919.py

--- Soal 1_D_71220919.py
+++ Soal 1_D_71220919.py
@@ -4,9 +4,26 @@ print ("ketik 3 untuk menghitung perkalian")
 print ("ketik 4 untuk menghitung pembagian")
 print ("ketik 5 untuk menghitung sisa hasil bagi")
 print ("ketik 6 untuk menghitung pemangkatan")
-x = int(input("masukan pilihan anda"))
-y = int(input("masukan bilangan pertama"))
-z = int(input("masukan bilangan kedua"))
-if x== '1' :
+x = int(input("masukan pilihan anda:"))
+y = int(input("masukan bilangan pertama:"))
+z = int(input("masukan bilangan kedua:"))
+if x== 1 :
     jumlah = y+z
-    print (f"hasil operasi bilangan {y} + {z}= {} ")
+    print ("hasil dari",y,"dijumlahkan dengan",z,"adalah",jumlah)
+elif x== 2 :
+    jumlah = y-z
+    print ("hasil dari",y,"dikurangi dengan",z,"adalah",jumlah)
+elif x== 3 :
+    jumlah = y*z
+    print ("hasil dari",y,"dikali dengan",z,"adalah",jumlah)
+elif x== 4 :
+    jumlah = y/z
+    print ("hasil dari",y,"dibagi dengan",z,"adalah",jumlah)
+elif x== 5 :
+    jumlah = y%z
+    print ("hasil dari",y,"sisa bagi dengan",z,"adalah",jumlah)
+elif x== 6 :
+    jumlah = y**z
+    print ("hasil dari",y,"dipangkatkan dengan",z,"adalah",jumlah)
+else :
+    print ("ramasokk")
