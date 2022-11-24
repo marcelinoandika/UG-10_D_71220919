f5b69794c8d27ca010e7f3004b98b45d72af3ee1
marcelinoandika <117901348+marcelinoandika@users.noreply.github.com>
2022-11-24 16:44:05 +0700

    Update Soal 2_D_71220919.py

--- Soal 2_D_71220919.py
+++ Soal 2_D_71220919.py
@@ -1,9 +1,1 @@
-sayang = int(input("Masukkan tahun: "))
-if sayang % 400 == 0:
-    print("{} merupakan tahun kabisat".format(sayang))
-elif sayang % 100 == 0:
-    print("{} bukan tahun kabisat".format(sayang))
-elif sayang % 4 == 0:
-    print("{} merupakan tahun kabisat".format(sayang))
-else:
-    print("{} bukan tahun kabisat".format(sayang))
+
